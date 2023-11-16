c7d64dd42687cab7ada583a2fd9c3751b30122d2
ingchrist <cdjinguet@gmail.com>
2023-11-16 23:46:50 +0100

    files added

--- main.c
+++ main.c
@@ -2,40 +2,40 @@
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
+* @wqzarc: number of arguments
+* @wqzgv: monty file location
 * Return: 0 on success
 */
-int main(int argc, char *argv[])
+int main(int wqzarc, char *wqzgv[])
 {
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	ssize_t read_line = 1;
+	ssize_t wzqrd_lne = 1;
 	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	unsigned int wzqcnt = 0;
 
-	if (argc != 2)
+	if (wqzarc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
+	file = fopen(wqzgv[1], "r");
 	bus.file = file;
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", wqzgv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	while (wzqrd_lne > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+		wzqrd_lne = getline(&content, &size, file);
 		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		wzqcnt++;
+		if (wzqrd_lne > 0)
 		{
-			execute(content, &stack, counter, file);
+			execute(content, &stack, wzqcnt, file);
 		}
 		free(content);
 	}
