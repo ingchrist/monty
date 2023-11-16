44ec646a3e0b42695da3864f3e763ca8c9e29752
ingchrist <cdjinguet@gmail.com>
2023-11-16 23:14:01 +0100

    files added

--- div.c
+++ div.c
@@ -1,40 +1,40 @@
 #include "monty.h"
 /**
  * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
+ * @wzqhd: stack wzqhd
+ * @wzqctr: line_number
  * Return: no return
 */
-void f_div(stack_t **head, unsigned int counter)
+void wqz_dv(stack_t **wzqhd, unsigned int wzqctr)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int wzqlen = 0, aux;
 
-	h = *head;
+	h = *wzqhd;
 	while (h)
 	{
 		h = h->next;
-		len++;
+		wzqlen++;
 	}
-	if (len < 2)
+	if (wzqlen < 2)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fprintf(stderr, "L%d: can't div, stack too short\n", wzqctr);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free_stack(*wzqhd);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
+	h = *wzqhd;
 	if (h->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
+		fprintf(stderr, "L%d: division by zero\n", wzqctr);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		free_stack(*wzqhd);
 		exit(EXIT_FAILURE);
 	}
 	aux = h->next->n / h->n;
 	h->next->n = aux;
-	*head = h->next;
+	*wzqhd = h->next;
 	free(h);
 }
