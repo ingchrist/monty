18c47c0d4ad5895d1911070fd1a65783b5791c99
ingchrist <cdjinguet@gmail.com>
2023-11-16 23:20:13 +0100

    files added

--- execute.c
+++ execute.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
-* execute - executes the opcode
+* wzqxct - wzqxcts the opcode
 * @stack: head linked list - stack
-* @counter: line_counter
+* @wzqcnt: line_wzqcnt
 * @file: poiner to monty file
-* @content: line content
+* @wzqctt: line wzqctt
 * Return: no return
 */
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+int wzqxct(char *wzqctt, stack_t **stack, unsigned int wzqcnt, FILE *file)
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
@@ -30,22 +30,22 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	unsigned int i = 0;
 	char *op;
 
-	op = strtok(content, " \n\t");
+	op = strtok(wzqctt, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
 	bus.arg = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, counter);
+		{	opst[i].f(stack, wzqcnt);
 			return (0);
 		}
 		i++;
 	}
 	if (op && opst[i].opcode == NULL)
-	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", wzqcnt, op);
 		fclose(file);
-		free(content);
+		free(wzqctt);
 		free_stack(*stack);
 		exit(EXIT_FAILURE); }
 	return (1);
