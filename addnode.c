6ddeb4db5d45ebb14247e53f8b4429dbcac5a7a0
ingchrist <cdjinguet@gmail.com>
2023-11-16 23:08:31 +0100

    files added

--- addnode.c
+++ addnode.c
@@ -1,24 +1,24 @@
 #include "monty.h"
 /**
- * addnode - add node to the head stack
- * @head: head of the stack
+ * addnode - add node to the wzqhd stack
+ * @wzqhd: wzqhd of the stack
  * @n: new_value
  * Return: no return
 */
-void addnode(stack_t **head, int n)
+void wzqdnd(stack_t **wzqhd, int n)
 {
 
-	stack_t *new_node, *aux;
+	stack_t *wzq_nd, *aux;
 
-	aux = *head;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	aux = *wzqhd;
+	wzq_nd = malloc(sizeof(stack_t));
+	if (wzq_nd == NULL)
 	{ printf("Error\n");
 		exit(0); }
 	if (aux)
-		aux->prev = new_node;
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+		aux->prev = wzq_nd;
+	wzq_nd->n = n;
+	wzq_nd->next = *wzqhd;
+	wzq_nd->prev = NULL;
+	*wzqhd = wzq_nd;
 }
