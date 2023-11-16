e59c2c73ea9bafdabca0769808935c8260529f3c
ingchrist <cdjinguet@gmail.com>
2023-11-16 23:21:28 +0100

    files added

--- free_stack.c
+++ free_stack.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-* free_stack - frees a doubly linked list
+* wqzfr_stack - frees a doubly linked list
 * @head: head of the stack
 */
-void free_stack(stack_t *head)
+void wqzfr_stack(stack_t *head)
 {
 	stack_t *aux;
 
